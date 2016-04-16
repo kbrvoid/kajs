2d2a0a35be00bf6cc087eea025d2c387259b9d36
KingPixil <simplecooldude1@gmail.com>
2016-04-16 09:01:28 -0700

    Update command.js

--- bin/command.js
+++ bin/command.js
@@ -159,6 +159,9 @@ function writeJs(source, js, base) {
         console.log('\x1b[36m', '======= KAJS =======' ,'\x1b[0m')
         console.log('\x1b[35m',source, '\x1b[32m', 'Compiled successfully! ✓', '\x1b[0m' ); 
       }
+      
+        console.log('\x1b[36m', '======= KAJS =======' ,'\x1b[0m')
+        console.log('\x1b[35m',source, '\x1b[32m', 'Compiled successfully! ✓', '\x1b[0m' );
     })
   }
   fs.exists(dir, function(exists) {
